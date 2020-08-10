56c5b67e8a383d8dc89c72d88cbe86adbac3edb8
colbyfayock <fayock@gmail.com>
2020-08-10 19:19:40 -0400

    Adding sass, updating nesting, updating title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="https://nextjs.org">Next.js</a> Sass Starter
         </h1>
 
         <p className={styles.description}>
